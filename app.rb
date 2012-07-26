592b18cd655d375e79039183788ec683fbb4fdb4
Andrea Mostosi <andrea.mostosi@zenkay.net>
2012-07-26 11:40:09 +0200

    resume redirect

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require "sinatra"
 get	"/" do
 	erb :index
 end
+
+get "/cv" do
+  redirect "http://resume.linkedinlabs.com/y4e481wph"
+end
+
+get "/curriculum" do
+  redirect "http://resume.linkedinlabs.com/y4e481wph"
+end
+
+get "/resume" do
+  redirect "http://resume.linkedinlabs.com/y4e481wph"
+end
