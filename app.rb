bfb9ee1e861271c0b2b101cee97dda06acd1c68c
Andrea Mostosi <andrea.mostosi@zenkay.net>
2012-10-04 20:03:37 +0200

    Internal links to social networks accounts

--- app.rb
+++ app.rb
@@ -15,3 +15,24 @@ end
 get "/resume" do
   redirect "http://resume.linkedinlabs.com/y4e481wph"
 end
+
+get "/twitter" do
+  redirect "https://twitter.com/#!/zenkay"
+end
+
+get "/linkedin" do
+  redirect "http://it.linkedin.com/in/andreamostosi"
+end
+
+get "/github" do
+  redirect "https://github.com/zenkay"
+end
+
+get "/facebook" do
+  redirect "https://www.facebook.com/andrea.mostosi"
+end
+
+get "/aboutme" do
+  redirect "http://about.me/andreamostosi"
+end
+
